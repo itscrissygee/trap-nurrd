a554bee5a9f1842378feeaa85bf16fad8703e0d0
itscrissygee <emailcrissygee@gmail.com>
2024-12-13 19:25:48 -0600

    udpate to json and vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  base: "/", // Change this from '/repositoryname/' to just '/'
 });
