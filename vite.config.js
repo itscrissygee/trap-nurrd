08cf5ad4863a3972e0cbc8e490d3a4d2bba95049
itscrissygee <emailcrissygee@gmail.com>
2024-12-13 19:19:53 -0600

    removed repository name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "itscrissygee/trap-nurrd", // Replace with your repository name
   plugins: [react()],
+  base: "/", // Change this from '/repositoryname/' to just '/'
 });
